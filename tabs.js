f344a99545315d930a4c7229f64debe8f31e57d1
cesar pagliacci <pagliaccicesar@gmail.com>
2024-02-21 11:19:43 -0300

    se le dio movimiento a desarrollo web

--- tabs.js
+++ tabs.js
@@ -29,3 +29,25 @@ const init = () => showTab(0);
 
 window.addEventListener('load',init,false);
 window.addEventListener('click',activate,false);
+
+
+
+// js para frase de avance
+
+const target = document.getElementById('shimmerWave');
+function splitTextToSpans(targetElement) {
+    if (targetElement) {
+        const text = targetElement.textContent;
+        targetElement.innerHTML = '';
+        for (let character of text) {
+            const span = document.createElement('span');
+            if (character === ' ') {
+                span.innerHTML = '&nbsp;';
+            } else {
+                span.textContent = character;
+            }
+            targetElement.appendChild(span);
+        }
+    }
+}
+splitTextToSpans(target);
