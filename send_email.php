f74b77c5f5741ea7316d85304fd125bc9618afd0
cesar pagliacci <pagliaccicesar@gmail.com>
2024-08-26 09:47:34 -0300

    se hace prueba de form

--- send_email.php
+++ send_email.php
@@ -1,11 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $to = "pagliaccicesar@gmail.com"; 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($nombre)) {
+        $to = "pagliaccicesar@gmail.com";  // Reemplaza con tu dirección de correo
         $subject = "Nuevo contacto";
-        $message = "Se ha registrado un nuevo correo: " . $email;
+        $message = "Nombre: " . $nombre . "\nCorreo: " . $email;
         $headers = "From: www.cesarpagliacci.com.ar";
 
         if (mail($to, $subject, $message, $headers)) {
@@ -18,4 +19,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
